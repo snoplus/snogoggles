03179d58e15675c3c10aaa60e21c8b7a79fb9ba3
Ken Clark <k.clark1@physics.ox.ac.uk>
2011-07-07 17:24:38 +0100

    Basic hist class.  Nothing worthwhile yet.

---
+++ src/Frames/Frames2d/Hist.cc
@@ -0,0 +1,37 @@
+
+#include <SFML/Graphics.hpp>
+#include <Viewer/Hist.hh>
+#include <Viewer/ImageManager.hh>
+#include <Viewer/ConfigurationTable.hh>
+#include <Viewer/ExitButton.hh>
+using namespace Viewer;
+using namespace Frames;
+
+void Hist::Initialise(ConfigurationTable& configTable) {
+  fHistBox = sf::Shape::Rectangle(0.0,0.0,0.5,0.5,sf::Color::Black,1.0f,sf::Color::Black);
+  //fHistBox.SetBoundingRect(0.0,0.0,1.0,1.0);
+  sf::Rect<double> button( 0.8, 0.1, 0.2, 0.1 );
+  fGUIManager.NewGUI<GUIs::ExitButton>(button);
+  fMessage = Text( "WTF?" );
+  fMessage.SetBoundingRect( sf::Rect<double>( 0.5, 0.0, 0.5, 0.5 ) );
+  fMessage.SetColor( sf::Color( 0, 0, 0 ) );
+}
+
+void Hist::Render2d(RWWrapper& windowApp){
+  windowApp.Draw(fHistBox);
+  windowApp.Draw(fMessage);
+}
+
+void Hist::Render3d(){
+}
+
+void Hist::EventLoop(){
+  while(!fEvents.empty()){
+    fEvents.pop();
+  }
+}
+
+void Hist::SaveConfiguration(ConfigurationTable& configTable){
+  // I think nothing goes in here...
+}
+
---
+++ src/Frames/Frames2d/Hist.hh
@@ -0,0 +1,47 @@
+////////////////////////////////////////////////////////////////////////
+/// \class Viewer::Frames::Hist
+///
+/// \brief   Displays histogram of information from events
+///
+/// \author  Ken Clark <k.clark1@physics.ox.ac.uk>
+///
+/// REVISION HISTORY:\n
+///     5/07/11 : K. Clark - added \n
+///
+/// \detail  I'll get back to you on that.
+///
+////////////////////////////////////////////////////////////////////////
+
+#ifndef __Viewer_Hist__
+#define __Viewer_Hist__
+#endif
+
+#include <Viewer/Frame.hh>
+#include <Viewer/Shape.hh>
+#include <Viewer/Text.hh>
+
+namespace Viewer {
+  namespace Frames {
+
+    class Hist: public Frame {
+      
+    public:
+      void Initialise(ConfigurationTable& configTable);
+      virtual void Render2d(RWWrapper& windowApp);
+      virtual void Render3d();
+      virtual void EventLoop();
+      void SaveConfiguration(ConfigurationTable& configTable);
+      void LoadConfiguration(ConfigurationTable& configTable);
+      virtual std::string GetName(){return Hist::Name();}
+      static std::string Name(){return std::string("Hist");}
+
+    private:
+      Shape fHistBox;
+      //bool fState;
+      Text fMessage;
+    };
+
+
+
+  }//Viewer namespace
+}//Frames namespace
