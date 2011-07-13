d6123fcb155f33aa10ace56fc5882c54445915be
Olivia Wasalski <wasalski@triumf.ca>
2011-07-13 16:08:29 -0700

    Forgot a file in last commit.

---
+++ src/Frames/Frames3d/Geo/Polyhedrons/Geodesic3d.hh
@@ -0,0 +1,39 @@
+////////////////////////////////////////////////////////////////////////
+/// \class Viewer::Frames::Geodesic3d
+///
+/// \brief   
+///
+/// \author Olivia Wasalski <wasalski@triumf.ca>
+///			    <oliviawasalski@triumf.ca>
+///
+/// REVISION HISTORY:\n
+/// 	12/07/11 : Olivia Wasalski - First Revision, New File \n
+///
+/// \details 	
+///
+////////////////////////////////////////////////////////////////////////
+
+#ifndef __Viewer_Frames_Geodesic3d__
+#define __Viewer_Frames_Geodesic3d__
+
+#include <Viewer/Polyhedrons3d.hh>
+#include <string>
+
+namespace Viewer {
+namespace Frames {
+
+class Geodesic3d : public Polyhedrons3d {
+
+public:
+
+    static std::string Name() { return "Geodesic"; }
+    std::string GetName() { return Name(); }
+
+    std::string GetFilename() { return "data/geodesic.xml"; }
+
+}; // class Geodesic3d
+
+}; // namespace Frames 
+}; // namespace Viewer
+
+#endif // __Viewer_Frames_Geodesic3d__
