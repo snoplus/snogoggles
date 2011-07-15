fd08480b463fa022f7088e31fda2f88f4c95d599
Olivia Wasalski <wasalski@triumf.ca>
2011-07-15 13:36:16 -0700

    Seems like the bug in configuration is fixed, restoring Geodesic geometry.

--- src/Frames/Frames3d/Geo/Polyhedrons3d.cc
+++ src/Frames/Frames3d/Geo/Polyhedrons3d.cc
@@ -4,22 +4,20 @@
 #include <Viewer/ConfigurationTable.hh>
 #include <Viewer/ConfigTableUtils.hh>
 #include <string>
-#include <iostream>
-#include <sstream>
 
 namespace Viewer {
 namespace Frames {
 
 void Polyhedrons3d::LoadFile()
 {
-/*
+
     Configuration config = Configuration( GetFilename(), false );
 
     std::vector< ConfigurationTable* >::iterator itr;
     for( itr = config.GetTableBegin(); itr != config.GetTableEnd(); itr++ )
         fPolyhedrons.push_back( ConfigTableUtils::GetPolyhedron( *itr ) );
 
-
+/*
     Configuration outConfig = Configuration( "data/geodesic2.xml", true );
     for( int i = 0; i < fPolyhedrons.size(); i++ )
         ConfigTableUtils::SetPolyhedron( outConfig.NewTable( "polyhedron" ), fPolyhedrons.at(i) );
