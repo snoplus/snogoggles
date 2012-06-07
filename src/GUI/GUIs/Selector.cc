fc9f558f83c96ac547b438b8c3e6f123475bd0e6
Phil Jones <p.g.jones@qmul.ac.uk>
2012-06-07 11:00:46 +0100

    New GUI Selector object.

--- src/GUI/GUIs/Selector.cc
+++ src/GUI/GUIs/Selector.cc
@@ -1,26 +1,36 @@
 using namespace std;
 
-#include <SFML/Graphics/Texture.hpp>
-
 #include <Viewer/Selector.hh>
 #include <Viewer/RWWrapper.hh>
 #include <Viewer/GUIProperties.hh>
-#include <Viewer/Sprite.hh>
-#include <Viewer/Text.hh>
+#include <Viewer/Button.hh>
 using namespace Viewer;
 using namespace Viewer::GUIs;
 
 Selector::Selector( RectPtr rect, 
                     unsigned int guiID )
-  : GUI( rect, guiID ), fText( RectPtr( rect->NewDaughter() ) ), fGUIManager( rect )
+  : GUI( rect, guiID ), fText( RectPtr( rect->NewDaughter() ) ), fGUIManager( rect ), fBackground( RectPtr( rect->NewDaughter() ) )
 {
+  fState = 0;
+  /// Initialise the text, left button, right button and background in that order
   sf::Rect<double> size = rect->GetRect( Rect::eResolution );
+  const double buttonWidth = size.Height; // Button size is defined by height
   sf::Rect<double> textSize = size;
-  textSize.Width = size.Width - 2.0 * size.Height;
-  textSize.Left = size.Height + size.Left;
+  textSize.Width = size.Width - 2.0 * buttonWidth;
+  textSize.Left = buttonWidth + size.Left;
   fText.GetRect()->SetRect( textSize, Rect::eResolution );
-  string temp( "Check Box" );
-  fText.SetString( temp );
+  sf::Rect<double> buttonSize = size;
+  buttonSize.Width = buttonWidth;
+  GUIs::Button* temp = fGUIManager.NewGUI< GUIs::Button >( buttonSize, Rect::eResolution );
+  temp->Initialise( 20 );
+  buttonSize.Left = size.Left + size.Width - buttonWidth;
+  temp = fGUIManager.NewGUI< GUIs::Button >( buttonSize, Rect::eResolution );
+  temp->Initialise( 21 );
+  size.Left = size.Left + buttonWidth;
+  size.Width = size.Width - 2.0 * buttonWidth;
+  fBackground.GetRect()->SetRect( size, Rect::eResolution );
+  const GUITextureManager& textureManager = GUIProperties::GetInstance().GetGUITextures();
+  fBackground.SetTexture( textureManager.GetTexture( 22, eBase ) );
 }
 
 Selector::~Selector()
@@ -29,7 +39,7 @@ Selector::~Selector()
 }
 
 void
-Selector::Initialise( vector<string> options )
+Selector::Initialise( const vector<string>& options )
 {
   fOptions = options;
 }
@@ -37,13 +47,23 @@ Selector::Initialise( vector<string> options )
 GUIEvent 
 Selector::NewEvent( const Event& event )
 {
-  return fGUIManager.NewEvent( event );
+  GUIEvent returnedEvent = fGUIManager.NewEvent( event );
+  if( !returnedEvent.IsNULL() )
+    {
+      if( returnedEvent.fguiID == 0 ) // Next option
+        fState = ( fState + 1 ) % fOptions.size();
+       if( returnedEvent.fguiID == 1 ) // Prev option
+        fState = ( fState - 1 ) % fOptions.size();
+    }
 }
 
 void 
 Selector::Render( RWWrapper& renderApp )
 {
-
+  fGUIManager.Render( renderApp );
+  renderApp.Draw( fBackground );
   fText.SetColour( GUIProperties::GetInstance().GetGUIColourPalette().GetText() );
+  if( !fOptions.empty() )
+    fText.SetString( fOptions[fState] );
   renderApp.Draw( fText );
 }
--- src/GUI/GUIs/Selector.hh
+++ src/GUI/GUIs/Selector.hh
@@ -1,15 +1,15 @@
 ////////////////////////////////////////////////////////////////////////
 /// \class Viewer::GUIs::Selector
 ///
-/// \brief   Label with a check box next to it
+/// \brief   Select strings iteratively
 ///
 /// \author  Phil Jones <p.jones22@physics.ox.ac.uk>
 ///
 /// REVISION HISTORY:\n
-///     30/06/11 : P.Jones - First Revision, new file. \n
-///     07/06/12 : P.Jones - Second Revision, composite object.\n
+///     07/06/12 : P.Jones - First Revision, new file. \n
 ///
-/// \detail  As brief
+/// \detail  Allows user to select through a list of strings till the 
+///          required one is found.
 ///
 ////////////////////////////////////////////////////////////////////////
 
@@ -19,14 +19,12 @@
 #include <string>
 
 #include <Viewer/GUI.hh>
-#include <Viewer/RectPtr.hh>
 #include <Viewer/Text.hh>
 #include <Viewer/GUIManager.hh>
+#include <Viewer/Sprite.hh>
 
 namespace Viewer
 { 
-  class Button;
-  class RWWrapper;
 
 namespace GUIs
 {
@@ -34,24 +32,28 @@ namespace GUIs
 class Selector : public GUI
 {
 public:
+  /// Standard GUI Constructor
   Selector( RectPtr rect, unsigned int guiID );
+  /// Desctuctor
   virtual ~Selector();
-
-  void Initialise( std::vector<std::string> options );
-
+  /// Initialise with a vector of strings
+  void Initialise( const std::vector<std::string>& options );
+  /// New GUI Event
   GUIEvent NewEvent( const Event& event );
+  /// On render call
   void Render( RWWrapper& windowApp );
-  
+  /// Set the current state by position is option list
   inline void SetState( unsigned int state );
+  /// Get the current state by position
   inline unsigned int GetState() const;
+  /// Get the current state by string value
   inline std::string GetStringState() const;
 protected:
-  GUIManager fGUIManager;
-  std::vector<std::string> fOptions;
-  Button* fLeft;
-  Button* fRight;
-  Text fText;
-  unsigned int fState;
+  GUIManager fGUIManager; /// < GUI Manager, manages the buttons
+  std::vector<std::string> fOptions; /// < The string options
+  Text fText; /// < Displays the current option
+  unsigned int fState; /// < Current state position
+  Sprite fBackground; /// < Draws background to text
 };
 
 void
